55e16903e54b31e902bbd83988ad322fef3f20b8
Alexia Auddino <alexia.auddino@student.isae-supaero.fr>
2022-04-17 13:19:18 +0200

    Creation de la page d'authentification

--- topoguide/topoguide/settings.py
+++ topoguide/topoguide/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'topoguide.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'logout'
